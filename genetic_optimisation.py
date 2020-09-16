831cb3fb787bfb6c3a10816dfc4dd5ab0bfe2dcc
MBlogs <mjbrooks95@gmail.com>
2020-09-15 17:22:04 -0700

    Blog Version

--- genetic_optimisation.py
+++ genetic_optimisation.py
@@ -1,25 +1,21 @@
 import random
 
 
-def create_child(population, fitnesses, genes, tournament_size, mutation_rate):
-  # Select parents
-  parent1 = select(population, fitnesses, tournament_size)
-  parent2 = select(population, fitnesses, tournament_size)
-  # Crossover (breed) parents to create new individual
+def select(population, tournament_size):
+  # Choose an individual in population through winner of a tournament
+  assert tournament_size <= len(population)
+  tournament_winner_index = min(random.sample(range(len(population)), tournament_size))
+  parent = population[tournament_winner_index]
+  return parent.copy()
+
+
+def create_child(population, genes, tournament_size, mutation_rate):
+  # Breed two parents, returning (possibly mutated) offspring
+  parent1 = select(population, tournament_size)
+  parent2 = select(population, tournament_size)
   child = crossover(parent1, parent2)
-  # Mutate child (possibly)
   child = mutate(child, mutation_rate, genes)
   return child
-
-
-def select(population, fitnesses, tournament_size):
-  # Choose an individual in population by winner of a tournament
-  population_indexes = random.sample(range(len(population)),tournament_size)
-  tournament_population = [population[t] for t in population_indexes]
-  tournament_fitnesses = [fitnesses[t] for t in population_indexes]
-  tournament_winner = tournament_fitnesses.index(max(tournament_fitnesses))
-  parent = population[population_indexes[tournament_winner]]
-  return parent.copy()
 
 
 def crossover(parent1,parent2):
@@ -31,63 +27,60 @@ def crossover(parent1,parent2):
 
 
 def mutate(child, mutation_rate, genes):
+  # Choose to replace a gene with a random different one
   for gene in range(len(child)):
     if random.random() < mutation_rate:
       child[gene] = random.choice(genes)
   return child
 
 
-def population_fitnesses(population):
-  # Takes list of individuals and returns population and fitness lists (sorted by fitness)
-  fitnesses = [fitness(i) for i in population]
-  fitnesses, population = zip(*sorted(zip(fitnesses, population),reverse=True))
+def population_fitnesses(population, fitness_func):
+  # Takes list of individuals and returns population (sorted by fitness) and fitness lists
+  fitnesses = [fitness_func(i) for i in population]
+  fitnesses, population = zip(*sorted(zip(fitnesses, population), reverse=True))
   return list(population), list(fitnesses)
 
-def _fitness(i):
-  return sum(i)
 
-def fitness(i):
-  # Takes individual, returns fitness float. Needs to be customised according to use
-  ascii_diff = 0
-  for x in range(len("pass")):
-    ascii_diff -= abs(ord("pass"[x]) - ord(''.join(i)[x]))
-  return ascii_diff
-
-
-def optimise(iterations, genes, genome_length, population_size, elites, tournament_size, mutation_rate):
+def optimise(iterations, genes, genome_length, fitness_func
+             , population_size, elites_size, tournament_size, mutation_rate, max_fitness=None):
   # Initialisation and order by fitnesses
-  population = [random.sample(genes, genome_length) for _ in range(population_size)]
-  population, fitnesses = population_fitnesses(population)
+  population = [random.choices(genes, k=genome_length) for _ in range(population_size)]
+  population, fitnesses = population_fitnesses(population, fitness_func)
 
   # Iterate through generations of populations
   for generation in range(iterations):
     new_population = [p.copy() for p in population]
 
     # Keep the best elites, replace rest of population with new children
-    for p in range(elites, population_size):
-      new_population[p] = create_child(population, fitnesses, genes, tournament_size, mutation_rate)
+    for p in range(elites_size, population_size):
+      new_population[p] = create_child(population, genes, tournament_size, mutation_rate)
 
-    population, fitnesses = population_fitnesses(new_population)
-    print(f"Best individual {population[0]} with fitness {fitnesses[0]}")
+    # Compute fitnesses and sort population by them
+    population, fitnesses = population_fitnesses(new_population, fitness_func)
+    print(f"Gen: {generation+1}, Best: {population[0]} with Fitness: {fitnesses[0]}")
+
+    # Check if we've passed the max fitness stopping condition
+    if max_fitness is not None and fitnesses[0] >= max_fitness:
+      break
 
   return population[0], fitnesses[0]
 
-if __name__ == "__main__":
-  iterations = 100
-  genes = "abcdefghijklmnopqrstuvwxyz"
-  genome_length = 4
-  #genes = [1,2,3,4,5,6,7,8,9,0]
-  #genome_length = 4
-  population_size = 50
-  elites = 2
-  tournament_size = 4
-  mutation_rate = 0.05
-  best, best_fitness = optimise(iterations, genes, genome_length, population_size, elites, tournament_size, mutation_rate)
-  # population = [random.sample(genes, genome_length) for _ in range(population_size)]
-  # population, fitnesses = population_fitnesses(population)
-  # print(f"Population: {population}")
-  # print(f"Fitnesses: {fitnesses}")
-  # parent1 = select(population,fitnesses,tournament_size)
-  # parent2 = select(population, fitnesses, tournament_size)
-  # child = crossover(parent1,parent2)
 
+if __name__ == "__main__":
+  iterations = 20
+  population_size = 200
+  elites_size = 5
+  tournament_size = 10
+  mutation_rate = 0.1
+
+  genes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+  genome_length = 10
+  fitness_func = lambda x: sum(x)
+
+  best, best_fitness = optimise(iterations, genes, genome_length, fitness_func
+                                , population_size, elites_size, tournament_size, mutation_rate)
+  print(f"Best: {best} with Fitness: {best_fitness}")
+
+#genes = "abcdefghijklmnopqrstuvwxyz123456789"
+#genome_length = 24
+#fitness_func = lambda x: sum([-abs(ord("pleasedontcrackmepass123"[i]) - ord(''.join(x)[i])) for i in range(len(x))])
